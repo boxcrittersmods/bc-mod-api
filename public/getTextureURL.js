0d62b35001ea2d6927baba84164003a5f867fe6d
Tumble <tumblegamer@gmail.com>
2019-08-04 16:11:02 +0100

    version info

--- public/getTextureURL.js
+++ public/getTextureURL.js
@@ -3,6 +3,6 @@
  * @param {{name:string,label:string,required:bool,hidden:bool,site:string,category:string,default:string}} texture 
  * @param {object} sites 
  */
-function getTextureUrl(texture, sites) {
+function getTextureUrl(texture, sites,versionInfo) {
     return "";
 }
