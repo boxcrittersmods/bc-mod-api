5f85de0dca06d4878994b0459439ce78424b23f9
Tumble <tumblegamer@gmail.com>
2019-11-25 12:54:59 +0000

    app not router

--- src/app.js
+++ src/app.js
@@ -30,8 +30,8 @@ app.set("json spaces", 2);
  */
 
 //enable CORS
-router.use(cors());
-router.use(function(req, res, next) {
+app.use(cors());
+app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header(
 		"Access-Control-Allow-Headers",
