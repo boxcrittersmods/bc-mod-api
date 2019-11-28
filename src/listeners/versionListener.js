0622fcbafff72e63fd73ce8e053bcfc60188441c
Tumble <tumblegamer@gmail.com>
2019-11-28 20:23:37 +0000

    stuff

--- src/listeners/versionListener.js
+++ src/listeners/versionListener.js
@@ -1,6 +1,6 @@
 const bcVersions = require("#src/boxcritters/versions");
-const github = require('#src/util/github');
-const WebhookManager = require('#src/util/webhook');
+const github = require("#src/util/github");
+const WebhookManager = require("#src/util/webhook");
 
 var _sha = "";
 var whUrl = process.env.DISCORD_WEBHOOK;
@@ -8,41 +8,54 @@ var webhook = new WebhookManager("versions");
 webhook.AddListener(whUrl);
 
 async function Init() {
-    var { v, s } = await github.loadVersions();
-    if (!v) return;
-    _sha = s;
-    bcVersions.SetVersions(v);
+	var { v, s } = await github.loadVersions();
+	if (!v) return;
+	_sha = s;
+	bcVersions.SetVersions(v);
 }
 
 function SaveToGithub() {
-    var v = bcVersions.GetVersions();
-    github.saveVersions(v,_sha);
+	var v = bcVersions.GetVersions();
+	github.saveVersions(v, _sha);
 }
 
 var listeners = (() => {
-    function NewClient(n, v) {
-        webhook.Invoke("New Client Version", {
-            "client": v.n,
-            "itemsFolder":v.i
-        })
-        SaveToGithub(v);
-    }
-    function NewItems(i,v) {
-        webhook.Invoke("New Items Folder", {
-            "client": v.n,
-            "itemsFolder":v.i
-        });
-        SaveToGithub();
-    }
-    return {
-        NewClient,
-        NewItems
-    }
+	function NewClient(n, v) {
+		webhook.Invoke("New Client Version", {
+			client: v.n,
+			itemsFolder: v.i
+		});
+		SaveToGithub(v);
+	}
+	function NewItems(i, v) {
+		webhook.Invoke("New Items Folder", {
+			client: v.n,
+			itemsFolder: v.i
+		});
+		SaveToGithub();
+	}
+	return {
+		NewClient,
+		NewItems
+	};
 })();
 
-bcVersions.versionEvents.addEventListener("newClient",listeners, listeners.NewClient);
-bcVersions.versionEvents.addEventListener("newItems", listeners, listeners.NewItems);
+bcVersions.versionEvents.addEventListener(
+	"newClient",
+	listeners,
+	listeners.NewClient
+);
+bcVersions.versionEvents.addEventListener(
+	"newItems",
+	listeners,
+	listeners.NewItems
+);
 
-module.exports = {
-    Init
-};
+github.init().then(() => {
+    Init().then(() => {
+        console.log("yea");
+        
+    }).catch(e => {
+		console.log(e);
+	});
+});
