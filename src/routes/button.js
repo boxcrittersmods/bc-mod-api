8c9c7d4a7045ef336811df5051781ca59b7a8a7b
Alvarito050506 <donfrutosgomez@gmail.com>
2020-05-11 00:01:17 -0300

    Better buttons 2.

--- src/routes/button.js
+++ src/routes/button.js
@@ -19,28 +19,20 @@ function send_response(settings, res)
 				return;
 			}
 			res.set("Content-Type", "image/svg+xml");
-			var version = sub_body.match(/\/\/\s*@version\s+(.*)\s*\n/i)[1];
-			res.send(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${90 + 10 * (0.75 * (version.length + 1))}" height="20">
-						<linearGradient id="s" x2="0" y2="100%">
-							<stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
-							<stop offset="1" stop-opacity=".1"/>
-						</linearGradient>
-						<clipPath id="r">
-							<rect width="${90 + 10 * (0.75 * (version.length + 1))}" height="20" rx="3" fill="#fff"/>
-						</clipPath>
-						<g clip-path="url(#r)">
-							<rect width="${90 + 10 * (0.75 * (version.length + 1))}" height="20" fill="#555"/>
-							<rect x="90" width="${10 * (0.75 * (version.length + 1))}" height="20" fill="#10ca90"/>
-							<rect width="90" height="20" fill="url(#s)"/>
-						</g>
-						<g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
-							<text x="460" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="825">BCMC Published!</text>
-							<text x="460" y="140" transform="scale(.1)" textLength="825">BCMC Published!</text>
-							<text x="${1024 + 10 * (1.5 * (version.length + 1))}" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="${(version.length + 1) * 58}">${"v" + version}</text>
-							<text x="${1024 + 10 * (1.5 * (version.length + 1))}" y="140" transform="scale(.1)" textLength="${(version.length + 1) * 58}">${"v" + version}</text>
-						</g>
-					</svg>`);
-				
+			var version = sub_body.match(/\/\/\s*@version\s+(.*)\s*\n/i);
+			if (version)
+			{
+				request({
+					"url": `https://img.shields.io/badge/BCMC_Published!-v${version[1]}-10ca90`
+				}, function (sub_err, sub_res, sub_body) {
+					if (sub_err)
+					{
+						res.send(`Error: ${sub_err}.`);
+						return;
+					}
+					res.send(sub_body);
+				});
+			}
 		});
 	} catch (e) {
 		console.log(e);
@@ -73,12 +65,16 @@ router.use("/:url_or_name", async (req, res) => {
 					res.send(`Error: ${sub_err}.`);
 					return;
 				}
-				if (sub_body.match(/userscript:\s*(.*)\n/i)[1] == "true")
+				var userscript = sub_body.match(/userscript:\s*(.*)/i);
+				if (userscript && userscript[1] == "true")
 				{
-					url = sub_body.match(/install:\s*(.*)\n/i)[1];
-					send_response({
-						"url": url
-					}, res);
+					var install = sub_body.match(/install:\s*(.*)/i);
+					if (install)
+					{
+						send_response({
+							"url": install[1]
+						}, res);
+					}
 				} else
 				{
 					res.send("The mod is not a user script mod.");
@@ -86,8 +82,6 @@ router.use("/:url_or_name", async (req, res) => {
 				}
 			});
 		}
-
-		
 	} catch (e) {
 		console.log(e);
 		res.send("Error: " + e);
