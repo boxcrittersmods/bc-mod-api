daddc2b59448435ba69fb5c63d3c6e6a564b7525
Alvarito050506 <donfrutosgomez@gmail.com>
2020-05-10 22:51:24 -0300

    Trying to fix.

--- src/routes/button.js
+++ src/routes/button.js
@@ -13,28 +13,33 @@ function send_response(settings, res)
 {
 	try {
 		request(settings, function (sub_err, sub_res, sub_body) {
-				res.set("Content-Type", "image/svg+xml");
-				var version = sub_body.match(/\/\/\s*@version\s+(.*)\s*\n/i)[1];
-				res.send(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${90 + 10 * (0.75 * (version.length + 1))}" height="20">
-							<linearGradient id="s" x2="0" y2="100%">
-								<stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
-								<stop offset="1" stop-opacity=".1"/>
-							</linearGradient>
-							<clipPath id="r">
-								<rect width="${90 + 10 * (0.75 * (version.length + 1))}" height="20" rx="3" fill="#fff"/>
-							</clipPath>
-							<g clip-path="url(#r)">
-								<rect width="${90 + 10 * (0.75 * (version.length + 1))}" height="20" fill="#555"/>
-								<rect x="90" width="${10 * (0.75 * (version.length + 1))}" height="20" fill="#10ca90"/>
-								<rect width="90" height="20" fill="url(#s)"/>
-							</g>
-							<g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
-								<text x="460" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="825">BCMC Published!</text>
-								<text x="460" y="140" transform="scale(.1)" textLength="825">BCMC Published!</text>
-								<text x="${1024 + 10 * (1.5 * (version.length + 1))}" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="${(version.length + 1) * 58}">${"v" + version}</text>
-								<text x="${1024 + 10 * (1.5 * (version.length + 1))}" y="140" transform="scale(.1)" textLength="${(version.length + 1) * 58}">${"v" + version}</text>
-							</g>
-						</svg>`);
+			if (sub_err)
+			{
+				res.send(`Error: ${sub_err}.`);
+				return;
+			}
+			res.set("Content-Type", "image/svg+xml");
+			var version = sub_body.match(/\/\/\s*@version\s+(.*)\s*\n/i)[1];
+			res.send(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${90 + 10 * (0.75 * (version.length + 1))}" height="20">
+						<linearGradient id="s" x2="0" y2="100%">
+							<stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
+							<stop offset="1" stop-opacity=".1"/>
+						</linearGradient>
+						<clipPath id="r">
+							<rect width="${90 + 10 * (0.75 * (version.length + 1))}" height="20" rx="3" fill="#fff"/>
+						</clipPath>
+						<g clip-path="url(#r)">
+							<rect width="${90 + 10 * (0.75 * (version.length + 1))}" height="20" fill="#555"/>
+							<rect x="90" width="${10 * (0.75 * (version.length + 1))}" height="20" fill="#10ca90"/>
+							<rect width="90" height="20" fill="url(#s)"/>
+						</g>
+						<g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
+							<text x="460" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="825">BCMC Published!</text>
+							<text x="460" y="140" transform="scale(.1)" textLength="825">BCMC Published!</text>
+							<text x="${1024 + 10 * (1.5 * (version.length + 1))}" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="${(version.length + 1) * 58}">${"v" + version}</text>
+							<text x="${1024 + 10 * (1.5 * (version.length + 1))}" y="140" transform="scale(.1)" textLength="${(version.length + 1) * 58}">${"v" + version}</text>
+						</g>
+					</svg>`);
 				
 		});
 	} catch (e) {
@@ -63,6 +68,11 @@ router.use("/:url_or_name", async (req, res) => {
 			request({
 				"url": "https://raw.githubusercontent.com/boxcritters/boxcrittersmods.ga/master/_mods/" + req.params.url_or_name + ".md"
 			}, function (sub_err, sub_res, sub_body) {
+				if (sub_err)
+				{
+					res.send(`Error: ${sub_err}.`);
+					return;
+				}
 				if (sub_body.match(/userscript:\s*(.*)\n/i)[1] == "true")
 				{
 					url = sub_body.match(/install:\s*(.*)\n/i)[1];
