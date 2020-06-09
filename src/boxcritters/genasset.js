90fa1179c19375e208b67de15a07e380c571ec7f
Tumble <tumblegamer@gmail.com>
2020-06-09 18:10:30 +0100

    MORE DYNAMIC

--- src/boxcritters/genasset.js
+++ src/boxcritters/genasset.js
@@ -12,14 +12,23 @@ const shopJson = require('#data/shop.json');
 
 var SITE_URL = getSiteUrl();
 
-Object.defineProperty(Array.prototype, 'reduceAsync', {
-    value:  async function(action,def) {
-	var output = def;
-	for(var i in this) {
-		output = await action(output,this[i],i);
-	}
-	return output;
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function (search, this_len) {
+		if (this_len === undefined || this_len > this.length) {
+			this_len = this.length;
+		}
+		return this.substring(this_len - search.length, this_len) === search;
+	};
 }
+
+Object.defineProperty(Array.prototype, 'reduceAsync', {
+	value: async function (action, def) {
+		var output = def;
+		for (var i in this) {
+			output = await action(output, this[i], i);
+		}
+		return output;
+	}
 });
 
 function dynamicSort(property) {
@@ -50,26 +59,33 @@ function camelize(str) {
 		return index == 0 ? word.toLowerCase() : word.toUpperCase();
 	}).replace(/\s+/g, '');
 }
-
-function explode(obj,prefix) {
-	return Object.keys(obj).reduce((pieces,key)=>{
-		var value = obj[key];
-		if(typeof(prefix)=="string") key = prefix+"_"+key;
-		if(Array.isArray(value)) return Object.assign(pieces,value.reduce((arrObj,item,i)=>{
-			var suffix = value.length>1?i:"";
-			arrObj[key + suffix] = item;
-			return arrObj;
-		},{}))
-		var p = undefined;
-		if(prefix) p = key;
-		if(typeof value == "object") return Object.assign(pieces,explode(value,p));
-		pieces[key] = value;
-		return pieces
-	},{});
+function titleize(str) {
+	return str.replace(
+		/\w\S*/g,
+		function (txt) {
+			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+		}
+	);
 }
 
-function GetClientScript()
-{
+function explode(obj, prefix) {
+	return Object.keys(obj).reduce((pieces, key) => {
+		var value = obj[key];
+		if (typeof (prefix) == "string") key = prefix + "_" + key;
+		if (Array.isArray(value)) return Object.assign(pieces, value.reduce((arrObj, item, i) => {
+			var suffix = value.length > 1 ? i : "";
+			arrObj[key + suffix] = item;
+			return arrObj;
+		}, {}))
+		var p = undefined;
+		if (prefix) p = key;
+		if (typeof value == "object") return Object.assign(pieces, explode(value, p));
+		pieces[key] = value;
+		return pieces
+	}, {});
+}
+
+function GetClientScript() {
 	return "https://play.boxcritters.com/lib/client.min.js";
 }
 
@@ -82,7 +98,7 @@ function getSiteUrl(site = 'play.boxcritters') {
 }
 
 async function fillURL(url) {
-	if(!url) return "";
+	if (!url) return "";
 	//var paths = await BoxCritters.GetPaths();
 	var base = SITE_URL;
 	if (urlIsRoot(url)) {
@@ -92,22 +108,22 @@ async function fillURL(url) {
 	}
 }
 
-async function getSprites(spriteSheet,name) {
+async function getSprites(spriteSheet, name) {
 	var host = getSiteUrl("play.boxcritters");
 	var url = host + spriteSheet;
 	var website = Website.Connect(url);
 	var sprites = await website.getJson();
 
-	return sprites.images.reduceAsync(async (tp,sprite,i) =>{
+	return sprites.images.reduceAsync(async (tp, sprite, i) => {
 		var value = await fillURL(sprite);
 		var key = name;
-		if(sprites.images.length<=1) {
+		if (sprites.images.length <= 1) {
 			return value;
 		}
-		key +="_"+i;
+		key += "_" + i;
 		tp[key] = value;
 		return tp;
-	},{});
+	}, {});
 }
 
 async function getAssetInfo(type, site = 'play.boxcritters') {
@@ -136,13 +152,13 @@ async function GetCritters() {
 	var mascots = await getAssetInfo('mascots');
 	var tp = {};
 	tp = await critters.reduceAsync(async (tp, critterData) => {
-		console.log("Critter: " +critterData.critterId);
-		tp[critterData.critterId] = await getSprites(critterData.spriteSheet,critterData.critterId);
+		console.log("Critter: " + critterData.critterId);
+		tp[critterData.critterId] = await getSprites(critterData.spriteSheet, critterData.critterId);
 		return tp;
 	}, {});
 	tp.mascots = await mascots.reduceAsync(async (tp, critterData) => {
-		console.log("Mascot: " +critterData.critterId);
-		tp[critterData.critterId] = await getSprites(critterData.spriteSheet,critterData.critterId);
+		console.log("Mascot: " + critterData.critterId);
+		tp[critterData.critterId] = await getSprites(critterData.spriteSheet, critterData.critterId);
 		return tp;
 	}, {});
 	return tp;
@@ -157,38 +173,38 @@ async function GetCritters() {
 async function GetItems() {
 	var itemsData = await getAssetInfo('items');
 	var sheetsUsed = [];
-	var tp = itemsData.reduceAsync(async (tp,itemData) => {
-		if(!sheetsUsed.includes(itemData.spriteSheet)) {
+	var tp = itemsData.reduceAsync(async (tp, itemData) => {
+		if (!sheetsUsed.includes(itemData.spriteSheet)) {
 			sheetsUsed.push(itemData.spriteSheet)
 			var spriteSheetParts = itemData.spriteSheet.split("/");
-			var series = "items_"+spriteSheetParts[3];
-			console.log("ItemSheet: " +series);
-			tp[series] = await getSprites(itemData.spriteSheet,series);
+			var series = "items_" + spriteSheetParts[3];
+			console.log("ItemSheet: " + series);
+			tp[series] = await getSprites(itemData.spriteSheet, series);
 		}
 		return tp;
-	},{});
+	}, {});
 	return tp;
 }
 async function GetIcons() {
 	var itemsData = await getAssetInfo('items');
-	var tp = await itemsData.reduceAsync(async (tp,itemData) => {
-		console.log("Item: " +itemData.itemId);
+	var tp = await itemsData.reduceAsync(async (tp, itemData) => {
+		console.log("Item: " + itemData.itemId);
 
-		var theme = itemData.theme||'normal';
+		var theme = itemData.theme || 'normal';
 		var slot = itemData.slot;
-		tp[theme] = tp[theme]||{};
-		tp[theme][slot] = tp[theme][slot]||{};
+		tp[theme] = tp[theme] || {};
+		tp[theme][slot] = tp[theme][slot] || {};
 		tp[theme][slot][itemData.itemId] = await fillURL("/media/icons/" + itemData.itemId + ".png");
 
 		return tp;
-	},{});
+	}, {});
 	return tp;
 }
 
 async function GetRooms() {
 	var rooms = await getAssetInfo('rooms');
 	var tp = rooms.reduceAsync(async (tp, roomData) => {
-		console.log("Room: " +roomData.roomId);
+		console.log("Room: " + roomData.roomId);
 		var room = {}
 		let roomParts = { // probably change the name for this variable
 			background: "bg",
@@ -198,12 +214,12 @@ async function GetRooms() {
 			music: "music",
 		}
 		for (let i in roomParts)
-			if(roomData[i])
+			if (roomData[i])
 				room[roomData.roomId + "_" + roomParts[i]] = await fillURL(roomData[i]);
-		
-		if(roomData.spriteSheet)
+
+		if (roomData.spriteSheet)
 			room[roomData.roomId + "_sprites"] = await getSprites(roomData.spriteSheet, roomData.roomId + "_sprites");
-		
+
 		tp[roomData.roomId] = room;
 		return tp;
 
@@ -213,18 +229,18 @@ async function GetRooms() {
 
 async function GetMedia() {
 	var media = await getAssetInfo('media');
-	var tp = Object.keys(media).reduceAsync(async (tp,type)=>{
-		tp[type] = await getSprites(media[type].spriteSheet,type);
+	var tp = Object.keys(media).reduceAsync(async (tp, type) => {
+		tp[type] = await getSprites(media[type].spriteSheet, type);
 		return tp;
-	},{});
+	}, {});
 	return tp;
 }
 
 async function GetShop() {
-	var tp = Object.keys(shopJson).reduceAsync(async(tp,type)=>{
-		tp[type] = await getSprites(shopJson[type].spriteSheet,type);
+	var tp = Object.keys(shopJson).reduceAsync(async (tp, type) => {
+		tp[type] = await getSprites(shopJson[type].spriteSheet, type);
 		return tp;
-	},{});
+	}, {});
 	return tp;
 }
 
@@ -233,10 +249,129 @@ async function GetCritterBall() {
 	return tp;
 }
 
+async function getObjectSchematic(obj) {
+	obj = JSON.parse(JSON.stringify(obj));
+	//console.log("DOCUMENTING OBJECT<" + (Array.isArray(obj) ? ("array" +"("+obj.length+")") : typeof (obj)) + ">:", obj);
+	if (Array.isArray(obj)) {
+		if (obj.length > 1 && typeof (obj[0]) === "object") {
+			obj = Object.assign(...obj);
+		} else if (obj.length == 0) {
+			return "array";
+		} else {
+			obj = obj[0];
+		}
+	}
+	if (typeof (obj) === "object")
+		return Object.assign(...await Promise.all(Object.keys(obj).map(async k => ({ [k]: await getObjectSchematic(obj[k]) }))));
+	if (typeof (obj) === "string") {
+		if (obj.includes('.png'))
+			return 'texture';
+		if (obj.includes('.json')) {
+			/*var url = await fillURL(obj);
+			var website = Website.Connect(url);
+			var assetInfo = await website.getJson();
+			return await getObjectSchematic(assetInfo);*/
+			return "json";
+		}
+	}
+	return typeof (obj);
+}
+
 async function GetTextureData() {
 	//var symbols = await GetSymbols();
 
-	var tp = {
+	var manifests = await BoxCritters.GetManifests();
+
+	var idMap = {
+		mascot: "critter"
+	}
+	let tpAttribs = {
+		background: "bg",
+		foreground: "fg",
+		navMesh: "nm",
+		sprites:"spriteSheet"
+	}
+	let keyPriorities = [
+		'series',
+		'theme',
+		'slot'
+	]
+	var idSuffix = "Id";
+	var extentions = {
+		texture:".png",
+	}
+
+	var tp = Object.assign(
+		{
+			clientscript: await GetClientScript(),
+			misc: textureMisc,
+			manifests: await GetManifestLoc(),
+		},
+		await Object.keys(manifests).reduceAsync(async (tp, m) => {
+			var singular = m[m.length - 1] == "s" ? m.substr(0, m.length - 1) : m;
+			var mTitle = titleize(m);
+			var singleTitle = titleize(singular);
+			var assetInfo = await getAssetInfo(m);
+			console.log("== " + mTitle + " ==");
+			if (!Array.isArray(assetInfo)) {
+				if (assetInfo.spriteSheet) {
+					assetInfo = [assetInfo];
+				}
+				assetInfo = !assetInfo.spriteSheet ? Object.keys(assetInfo).map(k => Object.assign({ [singular + idSuffix]: k }, assetInfo[k])) : [assetInfo]
+			}
+			var types = await getObjectSchematic(assetInfo);
+
+			var mSpriteSheets = [];
+			console.log(types);
+			//console.log(assetInfo)
+			tp[m] = await assetInfo.reduceAsync(async (tp, asset) => {
+				var idKey = (idMap[singular] || singular) + idSuffix;
+				var assetName = asset[(idMap[singular] || singular) + idSuffix];
+				var assetTP = {};
+				console.log(singleTitle + ":", assetName);
+
+				//Sprite Sheet
+				if (asset.spriteSheet) {
+					assetTP[assetName + "_sprites"] = await getSprites(asset.spriteSheet, assetName);
+					mSpriteSheets.concat(assetTP[assetName + "_sprites"]);
+				}
+
+				//All textures tp the TP
+				for (var attribute in asset) {
+					if (typeof (asset[attribute]) !== "string")
+						continue;
+					var tpAttribute = tpAttribs[attribute] || attribute;
+					if (asset[attribute].includes(extentions.texture))
+						assetTP[assetName + "_" + tpAttribute] = await fillURL(asset[attribute]);
+				}
+
+				//Cheack for if theres one asset piece
+				if (Object.values(assetTP).length == 1) assetTP = Object.values(assetTP)[0];
+
+				//Herarchy setup
+				var assetHierachyParts = [];
+				for (var i in keyPriorities) {
+					var parent = assetHierachyParts[assetHierachyParts.length - 1] || tp;
+					var keyName = keyPriorities[i];
+					if (asset[keyName]) {
+						var key = asset[keyName];
+						assetHierachyParts[i] = parent[key] || {};
+						parent[key] = assetHierachyParts[i];
+					}
+				}
+				var placeInHierarchy = assetHierachyParts[assetHierachyParts.length - 1]||tp;
+				placeInHierarchy[assetName] = assetTP;
+				return tp;
+			}, {});
+			return tp;
+		}, {}),
+		{
+			shop: await GetShop(),
+			critterball: await GetCritterBall()
+		}
+	);
+
+	/*var tp = {
 		clientscript: await GetClientScript(),
 		misc: textureMisc,
 		manifests: await GetManifestLoc(),
@@ -248,13 +383,13 @@ async function GetTextureData() {
 		media: await GetMedia(),
 		shop: await GetShop(),
 		critterball: await GetCritterBall()
-	}
+	}*/
 	return tp;
 }
 
 async function GetTextureList(type) {
 	var things = await GetTextureData();
-	if(type) things = things[type];
+	if (type) things = things[type];
 	var tp = explode(things);
 	//tp.packVersion = "UNKNOWN";
 	return tp;
