516868670d6dfee42f4d7780940f19f75499b490
Tumble <tumblegamer@gmail.com>
2020-09-22 16:23:15 +0100

    update tracker

--- src/util/website.js
+++ src/util/website.js
@@ -1,4 +1,5 @@
-const request = require("request-promise");
+
+const fetch = require('node-fetch')
 const { JSDOM } = require("jsdom");
 
 function Website(body) {
@@ -11,23 +12,36 @@ function Website(body) {
 	//body.then(body => (w.body = body));
 }
 
-Website.Connect = function(url) {
-	var body = async () =>request(url);
+Website.Connect = function(url,body,method="GET") {
+	var body = body?
+	async()=>fetch(url,{
+		method,
+		body:JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+	}):
+	async()=>fetch(url);
 	var website = new Website(body);
 	return website;
 };
 
 Website.prototype.getJson = async function () {
-	var json = JSON.parse(await this.body());
+	var body = await this.body();
+	var json = body.json()
 	return json;
 }
 
 Website.prototype.getText = async function () {
-	return await this.body();
+	var body = await this.body();
+	return body.text();
+}
+
+Website.prototype.getBuffer = async function() {
+	var body = await this.body();
+	return body.buffer();	
 }
 
 Website.prototype.getDocument = async function() {
-	var { window } = new JSDOM(await this.body());
+	var { window } = new JSDOM(await this.getText());
 	var document = window.document;
 	return document;
 };
