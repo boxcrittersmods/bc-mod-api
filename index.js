c3df7c52a008c2c0219e5688f1b6429f3e133083
Tumble <tumblegamer@gmail.com>
2020-10-20 16:44:05 +0100

    enble the bot

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
-"use strict"
-require('module-alias/register')
+"use strict";
+require('module-alias/register');
 require('dotenv').config();
 
 
 {
 	const { Webhook } = require('discord-webhook-node');
-	let whUrl = process.env.DISCORD_WEBHOOK
-	let webhook = new Webhook(whUrl)
+	let whUrl = process.env.DISCORD_WEBHOOK;
+	let webhook = new Webhook(whUrl);
 	let oldLog = console.log;
 	let oldinfo = console.info;
-	let oldError= console.error;
+	let oldError = console.error;
 	let oldWarn = console.warn;
-	console.log = (...msg) =>{
+	console.log = (...msg) => {
 		oldLog(...msg);
-		msg = msg.map(m=>{
-			if(typeof(m)=="object") {
-				return "```json\n"+JSON.stringify(m)+"\n```"
+		msg = msg.map(m => {
+			if (typeof (m) == "object") {
+				return "```json\n" + JSON.stringify(m) + "\n```";
 			} return m;
-		})
-		msg = msg.join(" ")
-		if(msg.length>2000) return
-		webhook.send(msg)
-	}
-	
-	console.info = (...msg) =>{
+		});
+		msg = msg.join(" ");
+		if (msg.length > 2000) return;
+		webhook.send(msg);
+	};
+
+	console.info = (...msg) => {
 		oldinfo(...msg);
-		msg = msg.map(m=>{
-			if(typeof(m)=="object") {
-				return "```json\n"+JSON.stringify(m)+"\n```"
+		msg = msg.map(m => {
+			if (typeof (m) == "object") {
+				return "```json\n" + JSON.stringify(m) + "\n```";
 			} return m;
-		})
-		msg = msg.join(" ")
-		if(msg.length>2000) return
-		webhook.send("**Info:** "+msg)
-	}
-	console.error = (...msg) =>{
+		});
+		msg = msg.join(" ");
+		if (msg.length > 2000) return;
+		webhook.send("**Info:** " + msg);
+	};
+	console.error = (...msg) => {
 		oldError(...msg);
-		msg = msg.map(m=>{
-			if(typeof(m)=="object") {
-				return "```json\n"+JSON.stringify(m)+"\n```"
+		msg = msg.map(m => {
+			if (typeof (m) == "object") {
+				return "```json\n" + JSON.stringify(m) + "\n```";
 			} return m;
-		})
-		msg = msg.join(" ")
-		if(msg.length>2000) return
-		webhook.send("**Error:** "+msg)
-	}
-	console.warn = (...msg) =>{
+		});
+		msg = msg.join(" ");
+		if (msg.length > 2000) return;
+		webhook.send("**Error:** " + msg);
+	};
+	console.warn = (...msg) => {
 		oldWarn(...msg);
-		msg = msg.map(m=>{
-			if(typeof(m)=="object") {
-				return "```json\n"+JSON.stringify(m)+"\n```"
+		msg = msg.map(m => {
+			if (typeof (m) == "object") {
+				return "```json\n" + JSON.stringify(m) + "\n```";
 			} return m;
-		})
-		msg = msg.join(" ")
-		if(msg.length>2000) return
-		webhook.send("**Warning:** "+msg)
-	}
+		});
+		msg = msg.join(" ");
+		if (msg.length > 2000) return;
+		webhook.send("**Warning:** " + msg);
+	};
 }
 
-//require("bcmc-community-tracker");
+require("bcmc-community-tracker");
 
 const webserver = require("tn-webserver");
 const app = require('#src/app');
 
-let server = webserver(app);
+webserver(app);
 
 
 
