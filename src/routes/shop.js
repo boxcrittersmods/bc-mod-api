88651513b6eeea304c35a5a2e975b8c2f30e9af1
Tumble <tumblegamer@gmail.com>
2021-04-09 14:08:33 +0100

    tempory shop info

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -2,6 +2,9 @@
 const express = require("express");
 
 
+const shopInfo = require('#data/getShop.json');
+
+
 
 
 let router = express.Router();
@@ -9,7 +12,7 @@ let router = express.Router();
 router.use(express.json());
 
 router.get('/', async function (req, res) {
-    res.send({ coming: "soon" });
+    res.send(shopInfo);
 });
 
 module.exports = router;
